b8a46dcb4fec27c815c1b9c4c9650879fadc0acd
Oyanna Lopez <oyannalopez14@gmail.com>
2023-09-07 16:45:45 +0200

    function to create a book element

--- js/script.js
+++ js/script.js
@@ -9,4 +9,20 @@ async function fetchBooksData(url) {
     }
 }
 
+// Create a book element
+function createBookElement(book) {
+    // Copy template
+    const bookElement = document.importNode(document.getElementById('book-template').content, true);
+
+    // Put the title
+    bookElement.querySelector('.book-ttl').textContent = book.title;
+
+    // Put the image
+    const img = bookElement.querySelector('.book-img');
+    img.src = book.image;
+    img.alt = book.title;
+
+    return bookElement;
+}
+
 fetchBooksData('datas/books.json');
